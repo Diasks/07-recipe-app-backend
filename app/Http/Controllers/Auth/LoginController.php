2d325c42589f740a0d12189e003fd147feb293e2
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-02-18 20:08:58 +0100

    add function for login and logout with jwt token

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,39 +1,88 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use DB;
+use JWTAuth;
+use Validator;
+
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+     // Login for registered user, if successfull return JWT-token!
+ 
+    public function login(Request $request)
     {
-        $this->middleware('guest')->except('logout');
+        $credentials = $request->only('email', 'password');
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+        $validator = Validator::make($credentials, $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->messages()
+            ]);
+        }
+        try {
+            //  attempting to verify the credentials and create token for user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'We can`t find an account with this credentials.'
+                ], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to login, please try again.'
+            ], 500);
+        }
+        //if everything is working a jwt-token is returned
+        return response()->json([
+            'status' => 'success',
+            'data'=> [
+                'token' => $token,
+                'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()->name
+            ]
+        ]);
     }
+    
+ 
+     // When logging out, destroy token. User have to login again to get a new token.
+   
+    public function logout(Request $request)
+    {
+        $token = $request->header('Authorization');
+        try {
+            JWTAuth::invalidate($token);
+            return response()->json([
+                'status' => 'success',
+                'message'=> "User successfully logged out."
+            ]);
+        } catch (JWTException $e) {
+            return response()->json([
+              'status' => 'error',
+              'message' => 'Failed to logout, please try again.'
+            ], 500);
+        }
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+    
+
