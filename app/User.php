3ea1fa57dbd4b072ea625509628d2404673fc315
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-02-18 19:50:38 +0100

    create relation to list, implement obligated jwt functions

--- app/User.php
+++ app/User.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace App;
-
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable  implements JWTSubject
 {
     use Notifiable;
+
+    public function list() {
+      return $this->hasMany(RecipeList::class);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -27,4 +30,26 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // public function lists()
+    // {
+    //   return $this->hasMany(List::class);
+    // }
+  /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 }
