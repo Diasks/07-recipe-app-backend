1558a7e0c36e1146ca4ab0764b3c6c7284cb87e9
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-02-18 19:47:52 +0100

    update user controller with function to get all users and show users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function index()
+    {
+        //
+        return User::all();
+    }
+
+
+    public function show(User $user)
+    {
+        //
+        return $user;
+    }
+}
